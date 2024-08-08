cad9628b2e5bfe3a04017394f20f08fe57346d69
sonic <bernardnketia51@gmail.com>
2024-08-08 09:50:00 +0000

    created a react version

---
+++ React/personal-profile/src/components/Header.js
@@ -0,0 +1,12 @@
+// src/components/Header.js
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>My Personal Profile</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ React/personal-profile/src/components/Portfolio.js
@@ -0,0 +1,13 @@
+// src/components/Portfolio.js
+import React from 'react';
+
+const Portfolio = () => {
+  return (
+    <section>
+      <h2>My Portfolio</h2>
+      <p>Showcase your work here.</p>
+    </section>
+  );
+};
+
+export default Portfolio;
